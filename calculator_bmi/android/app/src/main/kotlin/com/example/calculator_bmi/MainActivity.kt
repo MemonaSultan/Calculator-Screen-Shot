898b1b148ec4652f5c1a1cac3a7f4cdaeddc864d
Memona Sultan <memonase256@gmail.com>
2024-11-09 20:27:08 +0500

    video 04(Theme Data)
    In this video, it explains how we can use theme data to set text colors and use the scaffold's background property to change the background color. The copy With property is used to shorten the code length. Additionally, a dart file was created named input page, and the Stateful and Stateless classes were separated.

--- calculator_bmi/android/app/src/main/kotlin/com/example/calculator_bmi/MainActivity.kt
+++ calculator_bmi/android/app/src/main/kotlin/com/example/calculator_bmi/MainActivity.kt
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 package com.example.calculator_bmi
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+=======
+package com.example.calculator_bmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
+>>>>>>> 82f790296eae87c95960c42c415702a169e0a0e4
