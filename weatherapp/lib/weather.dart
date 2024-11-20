ae66e5d3640b13f1d0818c73971db34c1e8b0c26
Memona Sultan <memonase256@gmail.com>
2024-11-20 23:35:47 +0500

    Video 03(Add Images)
    Images
    1.Umbrella
    2.white snow
    3.raindrops

--- weatherapp/lib/weather.dart
+++ weatherapp/lib/weather.dart
@@ -18,6 +18,20 @@ class _WeatherState extends State<Weather> {
           )
         ],
       ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image(
+              image: AssetImage("images/umbrella.png"),
+              height: 1200.0,
+              width: 590.0,
+              fit:BoxFit.fill
+            ),
+          //  child: new Image.asset("images/umbrella.png"),
+          )
+
+        ],
+      ),
      );
 
   }
